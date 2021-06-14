0af1b8945e4d05492674ffe04b8dd93f1ec12333
Jake Lee <40153724+jlee0425@users.noreply.github.com>
2021-06-15 04:08:52 +0900

    added particles effect in the background

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
-import GlobalStyles from '../styles/globalStyles';
-import theme from '../styles/theme';
+import GlobalStyles from '@styles/globalStyles';
+import theme from '@styles/theme';
+import Particles from '@components/Particles';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<ThemeProvider theme={theme}>
 			<GlobalStyles />
+			<Particles />
 			<Component {...pageProps} />
 		</ThemeProvider>
 	);
