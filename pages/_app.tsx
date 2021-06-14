6f9df0e19dd5e79c8920e937b52a4a7b8ccc1676
Jake Lee <40153724+jlee0425@users.noreply.github.com>
2021-06-14 16:51:53 +0900

    global theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from '../styles/globalStyles';
+import theme from '../styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<ThemeProvider theme={theme}>
+			<GlobalStyles />
+			<Component {...pageProps} />
+		</ThemeProvider>
+	);
 }
-export default MyApp
+export default MyApp;
