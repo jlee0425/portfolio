24cf391bb7b0e96ae708d2a8a1bad60eedc4488b
Jake Lee <40153724+jlee0425@users.noreply.github.com>
2021-06-15 21:45:55 +0900

    added phone & restructured

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,34 +1,31 @@
 import { Icons } from '@components/Footer/Icons';
 import { Logo } from '@components/Header/Logo';
-import { Section, SectionText } from '@styles/styledComponents';
+import { Section, SectionDivider, SectionText } from '@styles/styledComponents';
 import React from 'react';
 import styled from 'styled-components';
 
 const index = () => {
 	return (
 		<Section id='contact'>
-			<Grid>
-				<Logo />
-				<div>
-					<a href='mailto:jlee425kr@gmail.com' style={{ paddingLeft: '1rem' }}>
-						<SectionText>jlee425kr@gmail.com</SectionText>
-					</a>
-					<Icons />
-				</div>
-			</Grid>
+			<Logo lg />
+			<Container>
+				<SectionText>+82-10-6860-3966</SectionText>
+				<a href='mailto:jlee425kr@gmail.com'>
+					<SectionText>jlee425kr@gmail.com</SectionText>
+				</a>
+				<SectionDivider style={{ margin: '2rem 0', width: '80px' }} />
+				<Icons />
+			</Container>
 		</Section>
 	);
 };
 
 export default index;
 
-const Grid = styled.div`
-	display: grid;
-	grid-template-columns: repeat(2, 1fr);
-	place-items: center;
-
-	@media ${({ theme }) => theme.breakpoints.sm} {
-		grid-template-columns: 1fr 1fr;
-		grid-template-rows: 2fr;
-	}
+const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	width: fit-content;
+	align-items: flex-start;
+	padding-left: 1rem;
 `;
