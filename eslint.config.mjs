65a916f6c4445a1efa31c90e4f0fbf77fedbb752
Jake Lee <jlee425kr@gmail.com>
2026-02-15 16:50:51 +0900

    styles: default theme

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,60 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import importPlugin from 'eslint-plugin-import';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+	...nextVitals,
+	...nextTs,
+	{
+		files: ['src/**/*.ts', 'src/**/*.tsx'],
+		plugins: {
+			import: importPlugin,
+		},
+		rules: {
+			'import/order': [
+				'error',
+				{
+					groups: [
+						'builtin',
+						'external',
+						'internal',
+						['parent', 'sibling'],
+						'index',
+					],
+					'newlines-between': 'always',
+					pathGroups: [
+						{
+							pattern: 'react',
+							group: 'external',
+							position: 'before',
+						},
+						{
+							pattern: '@/**',
+							group: 'internal',
+						},
+					],
+					pathGroupsExcludedImportTypes: ['react'],
+					alphabetize: {
+						order: 'asc',
+						caseInsensitive: true,
+					},
+				},
+			],
+			'import/no-duplicates': 'error',
+			'import/no-unused-modules': 'warn',
+			'import/first': 'error',
+			'import/newline-after-import': 'error',
+		},
+	},
+	// Override default ignores of eslint-config-next.
+	globalIgnores([
+		// Default ignores of eslint-config-next:
+		'.next/**',
+		'out/**',
+		'build/**',
+		'next-env.d.ts',
+	]),
 ]);
 
 export default eslintConfig;
